bbf2f295892eeef58b963e6c4d1416cfcf34332b
Hamsharan Mahalingam <hamsharan@gmail.com>
2024-03-06 14:28:00 -0500

    infinite run

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float moveSpeed = 3;
+    public float moveSpeed = 4;
     public float leftRightSpeed = 4;
 
     void Update()
